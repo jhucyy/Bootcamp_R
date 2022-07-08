43395496fccd9656cefdf55e1ee945b351fc3551
jhucyy <jhucyy.pereira@gmail.com>
2022-07-08 12:35:19 -0300

    modelagem de dados

--- modeling.R
+++ modeling.R
@@ -1,6 +1,17 @@
 # Modelagem de dados observacionais
 
 
-# Como fazer análises observacionais no R ---------------------------------
+# Como faze"./Wapo_Opioid_Data.Rdata")
+r análises observacionais no R ---------------------------------
 
+library(tidyverse)
+
+remedios <- opioid_data
+
+
+ggplot(remedios, aes(y = num_pills,
+                     x = republican_vote))+
+  geom_point()+
+  geom_smooth(method = "lm", formula = y = x)+
+  theme_classic()
 
